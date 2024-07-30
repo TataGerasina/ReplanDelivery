78c74540a95240fe3fb349cafbfa26b1bb19e8c8
Tatyana Gerasina <gtstata@gmail.com>
2024-07-30 17:06:56 +0300

    Made visible .jar

--- src/test/java/ru/netology/data/DataGenerator.java
+++ src/test/java/ru/netology/data/DataGenerator.java
@@ -34,6 +34,10 @@ public class DataGenerator {
         var faker = new Faker(new Locale(locale));
         return faker.phoneNumber().phoneNumber();
     }
+    public static String generateWrongPhone(String locale) {
+        var faker = new Faker(new Locale(locale));
+        return faker.numerify("#####");
+    }
 
     public static class Registration {
         private Registration() {
--- src/test/java/ru/netology/test/PatternsAllureTest.java
+++ src/test/java/ru/netology/test/PatternsAllureTest.java
@@ -9,8 +9,7 @@ import ru.netology.data.DataGenerator;
 
 import java.time.Duration;
 
-import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -21,6 +20,7 @@ class PatternsAllureTest {
     static void setUpAll() {
         SelenideLogger.addListener("allure", new AllureSelenide());
     }
+
     @AfterAll
     static void tearDownAll() {
         SelenideLogger.removeListener("allure");
@@ -32,13 +32,13 @@ class PatternsAllureTest {
     }
 
     @Test
-    @DisplayName("should Success All Forms")
-    void shouldSuccessAllForms() {
+    @DisplayName("should success plan and replan meeting ")
+    void shouldSuccessPlanMeeting() {
         var validUser = DataGenerator.Registration.generateUser("ru");
-        var daysAddForFirstMeeting = 4;
-        var firstMeetDate = DataGenerator.generateDate(daysAddForFirstMeeting);
-        var daysAddForSecondMeeting = 7;
-        var secondMeetDate = DataGenerator.generateDate(daysAddForSecondMeeting);
+        int daysAddForFirstMeeting = 4;
+        String firstMeetDate = DataGenerator.generateDate(daysAddForFirstMeeting);
+        int daysAddForSecondMeeting = 7;
+        String secondMeetDate = DataGenerator.generateDate(daysAddForSecondMeeting);
 
         $("[data-test-id='city'] input").setValue(validUser.getCity());
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
@@ -56,14 +56,33 @@ class PatternsAllureTest {
         $("[data-test-id='date'] input").setValue(secondMeetDate);
         $(byText("Запланировать")).click();
 
-       
-        $("[data-test-id='replan-notification'] .notification__content")
-                .shouldHave(exactText("У вас уже запланирована встреча на другую дату. Перепланировать?"))
-                .shouldBe(visible);
 
+        $("[data-test-id='replan-notification'] .notification__content")
+                .shouldHave(text("У вас уже запланирована встреча на другую дату. Перепланировать?"), Duration.ofSeconds(15))
+                .shouldBe(visible, Duration.ofSeconds(15));
         $("[data-test-id='replan-notification'] button").click();
         $("[data-test-id='success-notification'] .notification__content")
                 .shouldHave(exactText("Встреча успешно запланирована на " + secondMeetDate))
                 .shouldBe(visible);
+    }
+
+    @Test
+    @DisplayName("should get error message if wrong phone number entered")
+    void shouldGetErrorIfWrongPhoneNumber() {
+        var validUser = DataGenerator.Registration.generateUser("ru");
+        var daysAddForFirstMeeting = 4;
+        var firstMeetDate = DataGenerator.generateDate(daysAddForFirstMeeting);
+
+
+        $("[data-test-id='city'] input").setValue(validUser.getCity());
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(firstMeetDate);
+        $("[data-test-id='name'] input").setValue(validUser.getName());
+        $("[data-test-id='phone'] input").setValue(DataGenerator.generateWrongPhone("en"));
+        $("[data-test-id='agreement']").click();
+        $(byText("Запланировать")).click();
+
+        $("[data-test-id='phone'] .input_invalid .input__sub")
+                .shouldHave(exactText("Неверно введён номер мобильного телефона"));
     }
 }
